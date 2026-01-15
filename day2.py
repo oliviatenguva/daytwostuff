77267f61349a36694e6e7b70272dec8633ba3ddd
oliviatenguva <xad2vu@virginia.edu>
2026-01-15 20:07:14 +0000

    new

---
+++ day2.py
@@ -0,0 +1,24 @@
+# %%
+# Terminal Display Path: /workspaces/daytwostuff
+# 
+
+
+# %%
+# No, you should not. The virtual environments ae machine specific and can be recreated using the requirements.txt file.
+# 
+# %%
+# Terminal path after virtual environment: /workspaces/daytwostuff
+# The path is the same.
+# %%
+import pandas as pd
+df = pd.read_csv('pitching-sample.csv')
+print(df.head())
+
+# %%
+# It shows various extensions with similar names and/or functions to install.
+# Three Useful Elements of Data Wrangler: Data viewing, cleaning and preparation for tabular datasets
+# %%
+# Plotly version: 6.5.2
+
+# %%
+# We use a requirements.txt for reproducibility, easy set yp for collaboration, and easy deployment.
